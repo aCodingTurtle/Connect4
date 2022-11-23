e73af2062d6322ade502597163d4ee1b6023d42f
martinevard <evard.4@osu.edu>
2022-11-22 22:01:20 -0500

    Check for win linewise
    The general checkForWin function will stay the same but it can be done a couple of ways depending how we code the gameOver variable.

--- checkForWin.m
+++ checkForWin.m
@@ -1,5 +1,26 @@
 function [gameOver] = checkForWin(currentBoard)
+  if (chip1a == 1 && chip1b == 1 && chip1c == 1 && chip1d == 1)
+      gameOver = true;
+      return
+  else
+      gameOver = false;
+      return
+  end
+end
+gameOver = 0;
 %CheckForWin Checks board for a win (4 in a row) of either player color
-
-gameOver = true;
+%Check for win Row-wise
+for Row = 1:6
+        for Column = 1:4
+            if Row-win = checkForWin(currentBoard(Row, Column), currentBoard(Row, (Column+1)), currentBoard(Row, (Column+2)), currentBoard(Row, (Column+3)));
+                chip1 = 1;
+                gameOver = 1;
+            elseif
+                Row-win = checkForWin(currentBoard(Row, Column), currentBoard(Row, (Column+1)), currentBoard(Row, (Column+2)), currentBoard(Row, (Column+3)));
+                chip2 = 1;
+                gameOver = 1;
+            end
+            
+        end
+end
 end
