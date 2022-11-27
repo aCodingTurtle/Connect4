9c5b66e50103282de7bf3a10aa24c1f511b5a4e3
martinevard <evard.4@osu.edu>
2022-11-26 21:22:40 -0500

    All Check For Wins

--- checkForWin.m
+++ checkForWin.m
@@ -7,7 +7,10 @@ function [gameOver] = checkForWin(currentBoard)
       return
   end
 end
+
 gameOver = 0;
+chip1 = 0;
+chip2 = 0;
 %CheckForWin Checks board for a win (4 in a row) of either player color
 %Check for win Row-wise
 for Row = 1:6
@@ -15,12 +18,51 @@ for Row = 1:6
             if Row-win = checkForWin(currentBoard(Row, Column), currentBoard(Row, (Column+1)), currentBoard(Row, (Column+2)), currentBoard(Row, (Column+3)));
                 chip1 = 1;
                 gameOver = 1;
-            elseif
-                Row-win = checkForWin(currentBoard(Row, Column), currentBoard(Row, (Column+1)), currentBoard(Row, (Column+2)), currentBoard(Row, (Column+3)));
+            elseif Row-win = checkForWin(currentBoard(Row, Column), currentBoard(Row, (Column+1)), currentBoard(Row, (Column+2)), currentBoard(Row, (Column+3)));
                 chip2 = 1;
                 gameOver = 1;
             end
             
         end
 end
+
+%Check for win Column-wise
+for Ro = 1:3
+        for Colum = 1:7
+            if Column-win = checkForWin(currentBoard(Ro, Colum), currentBoard((Ro+1), Colum), currentBoard((Ro+2), Colum), currentBoard((Ro + 3), Colum));
+                chip1 = 1;
+                gameOver = 1;
+            elseif Column-win = checkForWin(currentBoard(Ro, Colum), currentBoard((Ro +1), Colum), currentBoard((Ro + 2), Colum), currentBoard((Ro + 3), Colum));
+                chip2 = 1;
+                gameOver = 1;
+            end
+            
+        end
 end
+
+%Check for Win Left Diagonal
+for DRow = 1:3
+        for DColumn = 1:4
+            if LeftD-win = checkForWin(game(DColumn, DRow), game((DRow+1), (DCoulmn+1)), game((DRow+2), (DColumn+2)), game((DRow+3), (DColumn+3)));
+                chip1 = 1;
+                gameOver = 1;
+            elseif LeftD-win = checkForWin(game(DColumn, DRow), game((DRow+1), (DCoulmn+1)), game((DRow+2), (DColumn+2)), game((DRow+3), (DColumn+3)));
+                chip2 = 1;
+                gameOver = 1;
+            end
+        end
+end
+
+%Check for Win Right Diagonal
+for DRo = 1:3
+        for DColum = 7:-1:4
+            if RightD-win = checkForWin(game(DColum, DRo), game((DRo+1), (DCoulm-1)), game((DRo+2), (DColum-2)), game((DRo+3), (DColum-3)));
+                chip1 = 1;
+                gameOver = 1;
+            elseif RightD-win = checkForWin(game(DColum, DRo), game((DRo+1), (DCoulm-1)), game((DRo+2), (DColum-2)), game((DRo+3), (DColum-3)));
+                chip2 = 1;
+                gameOver = 1;
+            end
+        end
+end
+
